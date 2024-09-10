ecf88c25c73997c9293ad0614071d829690e19d9
Alhaque <alhaque.khan880@gmail.com>
2024-09-10 10:30:29 +0530

    Did some more  changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Header from "@/components/Header";
 import { AiOutlineArrowUp, AiOutlineArrowDown, AiOutlineFilter } from "react-icons/ai";
+import { FaEthereum } from "react-icons/fa"; // Import Ethereum icon
+import { SiPolygon } from "react-icons/si"; // Import Matic (Polygon) icon
 
 export default function Swap() {
   return (
@@ -24,7 +26,14 @@ export default function Swap() {
                   className="flex-grow text-2xl -ml-3 outline-none border-none bg-gray-200"
                   placeholder="0.0"
                 />
-                <span className="text-black">ETH</span>
+                {/* Coin wrapper with grayish background and icon */}
+                <div className="flex items-center space-x-2 bg-gray-300 px-3 py-1 rounded-lg">
+                  {/* Ethereum icon with background color */}
+                  <div className="flex items-center justify-center bg-[#6F58F6] p-1 rounded-full">
+                    <FaEthereum size={16} className="text-white" />
+                  </div>
+                  <span className="text-black">ETH</span>
+                </div>
               </div>
               <div className="flex justify-between">
                 <div className="flex justify-end items-center w-full">
@@ -42,15 +51,15 @@ export default function Swap() {
             </div>
 
             {/* Arrow between sections */}
-            <div className="absolute inset-x-0 top-1/2 -mt-2 transform -translate-y-1/2 flex justify-center items-center">
+            <div className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 flex justify-center items-center -mt-4">
               <div className="flex flex-col items-center">
-                <AiOutlineArrowUp className="text-gray-400" />
-                <AiOutlineArrowDown className="text-gray-400" />
+                <AiOutlineArrowUp className="text-gray-400 -mt-10" size={20} /> {/* Smaller size */}
+                <AiOutlineArrowDown className="text-gray-400" size={20} /> {/* Smaller size */}
               </div>
             </div>
 
             {/* "You Receive" Section */}
-            <div className="mb-4 bg-gray-200 p-4 rounded-lg mt-12">
+            <div className="mb-4 bg-gray-200 p-4 rounded-lg mt-2">
               <label className="block text-black">You Receive</label>
               <div className="flex items-center space-x-2 bg-gray-200 rounded-lg px-4 py-2">
                 <input
@@ -58,7 +67,14 @@ export default function Swap() {
                   className="flex-grow text-2xl -ml-3 outline-none border-none bg-gray-200"
                   placeholder="0.0"
                 />
-                <span className="text-black">MATIC</span>
+                {/* Coin wrapper with grayish background and icon */}
+                <div className="flex items-center space-x-2 bg-gray-300 px-3 py-1 rounded-lg">
+                  {/* MATIC (Polygon) icon with background color */}
+                  <div className="flex items-center justify-center bg-[#A229C5] p-1 rounded-full">
+                    <SiPolygon size={16} className="text-white" />
+                  </div>
+                  <span className="text-black">MATIC</span>
+                </div>
               </div>
               <div className="text-right text-black text-sm mt-2">
                 Balance: 1500
